19b490b2d0c6c8ccc742cf5a196892204e4bc314
Alex Silva <alexcssilva@gmail.com>
2022-10-27 17:16:42 -0300

    feat: added analyze.py - Desafio 01 concluido

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,13 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if '.csv' not in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file) as csv_file:
+            file = csv.reader(csv_file)
+        return file
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
