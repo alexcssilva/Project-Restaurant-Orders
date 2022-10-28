21a18572b9085b0942caddc773cd89d821d12c96
Alex Silva <alexcssilva@gmail.com>
2022-10-27 21:14:03 -0300

    feat: added track_orders.py - Desafio 02 concluido

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,66 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self._queue = []
     # aqui deve expor a quantidade de estoque
+
     def __len__(self):
-        pass
+        return len(self._queue)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._queue.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders_list = []
+        for custom, order, day in self._queue:
+            if custom == customer:
+                orders_list.append(order)
+
+        orders_statistics = Counter(orders_list)
+        most_order = orders_statistics.most_common()
+
+        return most_order[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        list_orders = set()
+        total_orders = set()
+
+        for custom, order, day in self._queue:
+            total_orders.add(order)
+            if custom == customer:
+                list_orders.add(order)
+        result = total_orders - list_orders
+
+        return result
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        list_days = set()
+        total_days = set()
+
+        for custom, order, day in self._queue:
+            total_days.add(day)
+            if custom == customer:
+                list_days.add(day)
+        result = total_days - list_days
+
+        return result
 
     def get_busiest_day(self):
-        pass
+        list_days = []
+
+        for customer, order, day in self._queue:
+            if customer == customer:
+                list_days.append(day)
+
+        return max(list_days, key=list_days.count)
 
     def get_least_busy_day(self):
-        pass
+        list_days = []
+
+        for customer, order, day in self._queue:
+            if customer == customer:
+                list_days.append(day)
+
+        return min(list_days, key=list_days.count)
